be41a4639d3293309692e0cbd7f4f0ab701b6536
Matthieu Parent <matthieu.parent78@gmail.com>
2024-11-18 17:48:18 +0100

    rocket new create OK + form new

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -1,5 +1,5 @@
 class RocketsController < ApplicationController
-  before_action :set_rocket, only: %i[new create destroy]
+  before_action :set_rocket, only: %i[ show edit update destroy ]
 
   def index
     @rockets = Rocket.all
@@ -9,10 +9,19 @@ class RocketsController < ApplicationController
   end
 
   def new
+    @rocket = Rocket.new
   end
 
   def create
+    @rocket = Rocket.new(params_rocket)
+    @rocket.user = current_user
+    if @rocket.save
+      redirect_to rocket_path(@rocket)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
 
   def edit
   end
@@ -25,7 +34,11 @@ class RocketsController < ApplicationController
 
   private
 
+  def params_rocket
+    params.require(:rocket).permit(:name, :content, :price_per_day, :number_passengers, :img_url)
+  end
+
   def set_rocket
-    @rocket = Rocket.find(params[:rocket_id])
+    @rocket = Rocket.find(params[:id])
   end
 end

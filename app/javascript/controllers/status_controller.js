4a4840d70997ac2cbef3ecb2fae2b4e9ef6d00ac
Matthieu Parent <matthieu.parent78@gmail.com>
2024-11-22 13:50:44 +0100

    minor front fixes + js boutons acceper et rejeter booking

---
+++ app/javascript/controllers/status_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="status"
+export default class extends Controller {
+  static targets = ['currentstatus', 'divbutton']
+
+  connect() {
+    console.log(this.currentstatusTarget.textContent.slice(9))
+  }
+
+  accept (event) {
+    event.preventDefault();
+
+    const currentStatus = this.currentstatusTarget;
+    console.log(currentStatus);
+    currentStatus.innerText = 'Status : accepted';
+    this.divbuttonTarget.classList.add("d-none");
+  }
+
+  reject (event) {
+    event.preventDefault();
+
+    const currentStatus = this.currentstatusTarget;
+    console.log(currentStatus);
+    currentStatus.innerText = 'Status : rejected';
+    this.divbuttonTarget.classList.add("d-none");
+  }
+}
